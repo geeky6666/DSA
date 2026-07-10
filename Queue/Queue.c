f931b56aa449600423b7bfd5cf7f5f62e2403f5a
DijkstraDSA <codewithsurya052@gmail.com>
2026-07-11 05:00:22 +0530

    Add

--- Queue/Queue.c
+++ Queue/Queue.c
@@ -1,1 +1,59 @@
-xdhhhhhhhhhhhhhhhhhheeeeeeeeeeeee
+#include<stdio.h>
+#include<stdlib.h>
+struct queue{
+    int size;
+    int f;
+    int r;
+    int *arr;
+};
+int isFull(struct queue *q){
+    if(q->r==q->size-1){
+        return 1;
+    }
+    return 0;
+}
+int isEmpty(struct queue *q){
+    if(q->r==q->f){
+        return 1;
+    }
+    return 0;
+}
+void enqueue(struct queue *q,int val){
+    if(isFull(q)){
+        printf("Queue is Full \n");
+    }
+    else{
+        q->r++;
+        q->arr[q->r]=val;
+
+    }
+}
+int dequeue(struct queue *q){
+    int a=-1;
+    if(isEmpty(q)){
+        printf("Queue is Empty\n");
+    }
+    else{
+        q->f++;
+        a=q->arr[q->f];
+
+    }
+    return a;
+}
+int main()
+{
+    struct queue q;
+    q.size=10;
+    q.f=q.r=-1;
+    q.arr=(int *)malloc(q.size*sizeof(int));
+    if(isEmpty(&q)){
+        printf("Queue is empty\n");
+    }
+    enqueue(&q,10);
+    enqueue(&q,30);
+    printf("%d\n",dequeue(&q));
+    if(isFull(&q)){
+        printf("Queue is Full \n");
+    }
+    return 0;
+}
