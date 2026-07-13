e4dac7cbc3f782bfb1a0372c6eaa13bd2687923f
DijkstraDSA <codewithsurya052@gmail.com>
2026-07-14 04:29:40 +0530

    Add

--- 04_Queue/circular_queue.c
+++ 04_Queue/circular_queue.c
@@ -1,62 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-struct circularueue{
+struct circularQueue{
     int size;
     int f;
     int r;
     int *arr;
 };
-int isEmpty(struct circularueue *p){
-    if(p->r==p->f){
+int isEmpty(struct circularQueue *q){
+    if(q->r==q->f){
         return 1;
     }
     return 0;
 }
-int isFull(struct circularueue *p){
-    if((p->r+1)%p->size==p->f){
+int isFull(struct circularQueue *q){
+    if((q->r+1)%q->size==q->f){
         return 1;
     }
     return 0;
 }
-void enqueue(struct circularueue *p,int val){
-    if(isFull(p)){
-        printf("Queue is Full\n");
+void enqueue (struct circularQueue *q,int val){
+    if(isFull(q)){
+        printf("Queue is Full \n");
     }
     else{
-        p->r=(p->r+1)%p->size;
-        p->arr[p->r]=val;
+        q->r=(q->r+1)%q->size;
+        q->arr[q->r]=val;
     }
 }
-int dequeue(struct circularueue *p){
-    int a= -1;
-    if(isEmpty(p)){
+int dequeue(struct circularQueue *q){
+    int a=-1;
+    if(isEmpty(q)){
         printf("Queue is Empty\n");
     }
     else{
-        p->f=(p->f+1)%p->size;
-        a=p->arr[p->f];
+        q->f=(q->f+1)%q->size;
+        a=q->arr[q->f];
         return a;
-        
     }
 }
-int main()
-{
-    struct circularueue p;
-    p.size=10;
-    p.f=p.r=0;
-    p.arr=(int *)malloc(p.size*sizeof(int));
-    if(isEmpty(&p)){
-        printf("Queue is Empty \n");
+int main(){
+    struct circularQueue st;
+    st.size=10;
+    st.f=st.r=0;
+    st.arr=(int*)malloc(st.size*sizeof(int));
+    if(isEmpty(&st)){
+        printf("Queue is Empty\n");
     }
-    enqueue(&p,10);
-    enqueue(&p,20);
-    enqueue(&p,30);
-    printf("%d\n",dequeue(&p));
-    printf("%d\n",dequeue(&p));
-    if(isFull(&p)){
+    enqueue(&st,10);
+    enqueue(&st,20);
+    enqueue(&st,30);
+    printf("%d\n",dequeue(&st));
+    if(isFull(&st)){
         printf("Queue is Full \n");
+
     }
     return 0;
-
 }
